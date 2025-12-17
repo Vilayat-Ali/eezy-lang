3fdaafc3af84babb57eff46250e099240bcb9025
Vilayat-Ali <vilayatcodemysite@gmail.com>
2025-12-17 12:12:11 +0530

    feat: added boilerplate for clap.rs and added basic cli setup for build and run of the ez compiler cli

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,34 @@
 //! with the abstraction provided by languages like Python and Go, excluding unnecessary verbosity and
 //! enforcing better reachability among learners.
 
+use std::{path::PathBuf};
+
+use clap::Parser;
+use ezc::cli::{Cli, Commands};
+use colored::Colorize;
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let start = Instant::now();
+
+    match cli.command {
+        Commands::Run { source_path } => {
+            // run the source code
+            println!("Run the source code at {}",  source_path.iter().collect::<PathBuf>().display());
+        },
+        Commands::Build { source_path } => {
+            // build the source code
+            println!("Build the source code at {}", source_path.iter().collect::<PathBuf>().display())
+        }
+    };
+
+    let duration_ms = start.elapsed().as_millis();
+    let duration_msg = if duration_ms < 120 {
+        duration_ms.to_string().green()
+    } else {
+        duration_ms.to_string().red()
+    };
+
+    println!("Operation took {} ms", duration_msg);
 }
