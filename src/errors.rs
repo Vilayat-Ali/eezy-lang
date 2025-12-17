a42b36b0bc25ae9cec702f738aeee429c7707e08
Syed Vilayat Ali Rizvi <73014428+Vilayat-Ali@users.noreply.github.com>
2025-12-17 12:28:51 +0530

    fix: resolved lint errors (#6)

--- src/errors.rs
+++ src/errors.rs
@@ -3,5 +3,5 @@ use thiserror::Error;
 #[derive(Debug, Clone, Error)]
 pub enum EezyError<'a> {
     #[error("Invalid path to source {0} was provided.")]
-    InvalidSourcePath(&'a String)
+    InvalidSourcePath(&'a String),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-pub mod jvm;
-pub mod lexer;
-pub mod parser;
 pub mod cli;
 pub mod errors;
 pub mod input_validation;
+pub mod jvm;
+pub mod lexer;
+pub mod parser;
